807ecff8000a872bffbc47459ecc7fe23ab4097b
iliyapr <105923959+iliyapr@users.noreply.github.com>
2022-05-20 20:33:36 +0430

    Update Project.cpp

--- Project.cpp
+++ Project.cpp
@@ -1,16 +1,40 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include<bits/stdc++.h>
+using namespace std ;
+
+class Account 
+{
+	protected :
+	    // :)
+	    
+	public :
+		Account(int num_of_people  )
+		{
+
+		}
+
+		
+	
+	
+		
+};
+
+class Article : public Account
+{
+	string Name , ID , Ref_ID , Date ;
+	char *txt ;
+	
+	public :
+		Article(string name ,string id , string ref_id , string date ,string text, int n_people) : Account(n_people)
+		{
+			Name = name ; ID = id ; Ref_ID = ref_id ; Date = date ;
+			txt = &text[0];
+		}
+
+
+};
 
 int main()
 {
-
-
-
-
-
-
-
-
-
-    return 0 ;    
+	
+	return 0;
 }
