f254aee5e149db2ae67c2a31b51956344bdb800d
Soroush Beigi <sbeygi712@gmail.com>
2022-05-28 15:48:17 +0430

    huge update!
    modifying Article class and second case...adding a Validate class

--- Project.cpp
+++ Project.cpp
@@ -15,29 +15,45 @@ class Account
 
 class Article 
 {
+    protected:
+    int people_count;
 	string Name , ID , Ref_ID , Date ;
 	Account accounts[10];
 	char *txt ;
 	
 	public :
-		Article(string name ,string id , string ref_id , string date ,string text )
+		Article(string name ,string id , string ref_id , string date ,char *text,int people)
 		{
 			Name = name ; ID = id ; Ref_ID = ref_id ; Date = date ;
-			txt = &text[0];
+            people_count=people;
+			txt = text ;
 		}
 
-    void add_account(Account account,int n){
-        
-        
+    void add_account(Account account,int index){
+        accounts[index] = account;
     }    
 
 
 };
 
+class Validate : public Article{
+    public:
+       //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //!!!!!!Functiona ro inja benevis!!!!!!!
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+}
+
+
 int main()
 {
-	Account total_accounts[100][2];
-	static int num_accounts = 0;
+	Account total_accounts[100];
+	Article total_articles[100];
+
+	static int num_accounts = 0,num_articles=0;
 	char sit ; cout<<" start -> enter s : "; cin>>sit;
 	while(sit == 's')
 	{
@@ -53,6 +69,7 @@ int main()
 				cout<<"\n"<<"-1 : exit";
 				cout<<"\n"<<"0 : hint";
 				cout<<"\n"<<"1 : create account";
+				cout<<"\n"<<"2 : create article";
 				cout<<"\n";
 
 				break;
@@ -67,16 +84,29 @@ int main()
        		cout<<"Enter your Password: "<<endl;
        		cin>>pass;
        		cout<<endl;
-       		Account acc(user , pass) ;
-       		total_accounts[num_accounts] ; 
-       		num_accounts ++ ;
+       		Account acc(user , pass);
+       		total_accounts[num_accounts ++] = acc ; 
        
        
        break; }
        case 2:{
+            //create article
            int n;
+           string name,id,ref_id,date;
+           char text[40000];
+           cout<<"Enter Article's name: "<<endl;
+           getline(cin,name);
+           cout<<"Enter Article's ID: "<<endl;
+           cin>>id;
+           cout<<"Enter Article's reference ID: "<<endl;
+           cin>>ref_id;
+           cout<<"Enter Article's Date: "<<endl;
+           cin>>date;
+           cout<<"Enter Article's Text and add a '$' char in the end: "<<endl;
+           cin.get(text,sizeof(text),'$')
            cout<<"Enter participating people count: "<<endl;
            cin>>n;
+           Article article(name,id,ref_id,date,text,n);
            while(n>10 || n<=0){
                cout<<"Error: Enter a number between 1 and 10"<<endl;
                cin>>n;
@@ -88,9 +118,11 @@ int main()
                cout<<"Enter Person Number "<<(i+1)<<"'s Password: "<<endl;
                cin>>password;
                Account a(username,password);
+               article.add_account(a,i);
 
            }
 
+           total_articles[num_articles++];
 
            break;
        }
